5b46269861f4dadb790d075b4ef3965ad8c50126
Pawel <click1992@gmail.com>
2015-05-18 15:22:59 +0200

    Dodałem planszę

--- source/game.py
+++ source/game.py
@@ -3,14 +3,18 @@ import pygame
 
 
 class game():
-    def __init__(self, size=(640, 480)):
+    def __init__(self, size=(600, 600)):
         pygame.init()
         self.screen = pygame.display.set_mode(size)
+        self.load_assets()
 
+    def load_assets(self):
+        self.boardimage = pygame.image.load('assets/Plansza.bmp')
 
     def main_loop(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit()
+            self.screen.blit(self.boardimage, (0,0))
             pygame.display.flip()
